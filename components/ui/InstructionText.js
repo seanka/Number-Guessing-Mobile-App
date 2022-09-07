ab121dde8a37af700493cf987db4a436bff1d4e7
Sean Kristian Anderson <seankristian16@gmail.com>
2022-09-07 16:21:46 +0700

    added custom fonts

--- components/ui/InstructionText.js
+++ components/ui/InstructionText.js
@@ -2,14 +2,15 @@ import { Text, StyleSheet } from "react-native";
 
 import Colors from "../../constants/colors";
 
-function InstructionText({ children }) {
-  <Text style={styles.instructionText}>{children}</Text>;
+function InstructionText({ children, style }) {
+  <Text style={[styles.instructionText, style]}>{children}</Text>;
 }
 
 export default InstructionText;
 
 const styles = StyleSheet.create({
   instructionText: {
+    fontFamily: "open-sans",
     color: Colors.accent500,
     fontSize: 24,
   },
