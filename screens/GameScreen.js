bab44344b83933d7ed7e5bb0085b02483119a078
Sean Kristian Anderson <seankristian16@gmail.com>
2022-09-07 14:33:55 +0700

    added random number generator, fixed style in game screen, added number container component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
 
-import Title from "../components/title";
+import NumberContainer from "../components/game/NumberContainer";
+import Title from "../components/ui/title";
 
-function GameScreen() {
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
   return (
     <View style={styles.screen}>
       <View>
         <Title>Opponent's Guess</Title>
+        <NumberContainer>{currentGuess}</NumberContainer>
         <Text>Higher or Lower?</Text>
       </View>
       <View></View>
